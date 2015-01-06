ef097217cf7e11551d408efc9c3f65f03bbdb59f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-01-06 12:02:05 -0500

    initial commit

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+var assert = require('assert'),
+  toDot = require('../');
+
+describe('jsonpath to dot', function(){
+  it('should work', function(){
+    assert.equal(toDot('/tags'), 'tags');
+    assert.equal(toDot('/tags/1'), 'tags');
+    assert.equal(toDot('/tags/blah'), 'tags.blah');
+  });
+});
