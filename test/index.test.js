fc345330853e1309c05300ecd2899482ff03cb84
BenCo1967 <benco1967@gmail.com>
2017-07-19 18:21:09 +0200

    add escaped characters
    ~0 <=> ~and ~1 <=> /

--- test/index.test.js
+++ test/index.test.js
@@ -6,5 +6,6 @@ describe('jsonpath to dot', function(){
     assert.equal(toDot('/tags'), 'tags');
     assert.equal(toDot('/tags/1'), 'tags.1');
     assert.equal(toDot('/tags/blah'), 'tags.blah');
+    assert.equal(toDot('/foo~1bar~0'), 'foo/bar~');
   });
 });
